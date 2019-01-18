12eaecb0c5220a7d1fde584e8f61b95825f28cc9
yangjian <632639224@qq.com>
2019-01-18 19:22:13 +0800

    register_center

---
+++ register_center/src/test/java/com/jk/RegisterCenterApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RegisterCenterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
